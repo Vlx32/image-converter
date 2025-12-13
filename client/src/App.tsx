9f0eaa6bf119db5363a50d51aefd4527fe79655c
Vlx32 <129527577+Vlx32@users.noreply.github.com>
2025-12-13 21:51:07 +0300

    Add disabled state for format buttons during conversion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -103,6 +103,7 @@ function App() {
             <FormatSelector 
               selectedFormat={format}
               onFormatChange={setFormat}
+               disabled={loading} 
             />
 
             <button
